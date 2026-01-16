da1bef12d7ad36bfd69f1c6aedd35b603610a57d
in2dm3-d <in2dm3-d@elorrieta.int>
2026-01-16 12:55:10 +0100

    conexion socket funcional

--- src/main/java/com/ElorServ/ElorServ/socket/ClientHandler.java
+++ src/main/java/com/ElorServ/ElorServ/socket/ClientHandler.java
@@ -1,5 +1,53 @@
 package com.ElorServ.ElorServ.socket;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientHandler extends Thread {
+
+	private Socket clientSocket;
+	
+	public ClientHandler(Socket socket) {
+		this.clientSocket = socket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			
+			System.out.println("Profesor conectado desde: " + clientSocket.getInetAddress().getHostAddress());
+			
+			//aviso de conexion
+			out.println("Kaixo, ElorServ Socket zerbitzaria prest.");
+			
+			String inputLine;
+			
+			//bucle de comunicacion
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("Jaso den mezua: " + inputLine);
+				
+				//temporal para terminal conexion
+				if("AGUR".equalsIgnoreCase(inputLine.trim())) {
+					out.println("Agur, konexioa itxi da.");
+					break;
+				}
+				
+				//el servidor le responde al cliente
+				out.println("Zure mezua jaso da: " + inputLine);
+			}
+			
+			//cerrar recursos
+			in.close();
+			out.close();
+			clientSocket.close();
+			System.out.println("Conexion cerrada: " + clientSocket.getInetAddress().getHostAddress());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/main/java/com/ElorServ/ElorServ/socket/SocketServer.java
@@ -0,0 +1,38 @@
+package com.ElorServ.ElorServ.socket;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocketServer implements CommandLineRunner{
+
+	private static final int PORT = 9000;
+	
+	@Override
+	public void run(String... args)throws Exception{
+		
+		//lanzamos el servidor en un hilo separado para que no bloquee el arranque de Spring Boot
+		
+		new Thread(() -> {
+			try (ServerSocket serverSocket = new java.net.ServerSocket(PORT)) {
+				System.out.println("Socket zerbitzaria martxan portuan: " + PORT);
+				
+				while (true) {
+					//se queda congelado el server esperando conexiones
+					Socket clientSocket = serverSocket.accept();
+					
+					//cuando llega una conexion, se crea un nuevo hilo para manejarla
+					new ClientHandler(clientSocket).start();
+				}
+			} catch (IOException e) {
+				System.out.println("Errorea socket zerbitzaria abiaraztean: " + e.getMessage() + ", Portuan: "+ PORT);
+				e.printStackTrace();
+			}
+		}).start();
+	}
+	
+}
